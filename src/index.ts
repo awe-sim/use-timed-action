69457cbbef2d9c42aba8f07632b61e7076a17746
Asim Sohail <>
2023-06-09 03:08:04 +0500

    ..

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
 export { useCounter } from './hooks/useCounter/';
-export { useTimedActionV1, useTimedActionV2 } from './hooks/useTimedAction';
+export { useTimedAction } from './hooks/useTimedAction';
